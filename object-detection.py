cd51ba7ac7546c7c8170f30eddafa0620a29cc98
GiorgioDots <dodedodo96@gmail.com>
2020-04-14 15:37:21 +0200

    fixed vs does not exists

--- object-detection.py
+++ object-detection.py
@@ -59,35 +59,35 @@ def sendEvent():
 liveImageSender = LiveImagesSender()
 liveImageSender.run()
 
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=confidence,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# initialize the list of class labels MobileNet SSD was trained to
+# detect, then generate a set of bounding box colors for each class
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+# initialize the video stream, allow the cammera sensor to warmup,
+# and initialize the FPS counter
+print("[INFO] starting video stream...")
+print(resolution[0] + " - " + resolution[1])
+vs = VideoStream(src=0, usePiCamera=True, resolution=(int(resolution[0]),int(resolution[1]))).start()
+time.sleep(2.0)
+# fps = FPS().start()
+
 if isActive:
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-p", "--prototxt", required=True,
-		help="path to Caffe 'deploy' prototxt file")
-	ap.add_argument("-m", "--model", required=True,
-		help="path to Caffe pre-trained model")
-	ap.add_argument("-c", "--confidence", type=float, default=confidence,
-		help="minimum probability to filter weak detections")
-	args = vars(ap.parse_args())
-
-	# initialize the list of class labels MobileNet SSD was trained to
-	# detect, then generate a set of bounding box colors for each class
-	CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-		"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-		"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-		"sofa", "train", "tvmonitor"]
-	COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
-
-	# load our serialized model from disk
-	print("[INFO] loading model...")
-	net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
-	# initialize the video stream, allow the cammera sensor to warmup,
-	# and initialize the FPS counter
-	print("[INFO] starting video stream...")
-	print(resolution[0] + " - " + resolution[1])
-	vs = VideoStream(src=0, usePiCamera=True, resolution=(int(resolution[0]),int(resolution[1]))).start()
-	time.sleep(2.0)
-	# fps = FPS().start()
-
 	# loop over the frames from the video stream
 	while True:
 		# grab the frame from the threaded video stream and resize it
